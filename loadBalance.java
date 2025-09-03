4f20dcb553d9cc51a60738d643403f1d99a8c04c
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-03 18:39:50 +1000

    Create loadBalance.java
    Seatwork1Part1

---
+++ loadBalance.java
@@ -0,0 +1,10 @@
+public class loadBalance {
+  public static void main(String[] args) {
+    float loadBalance = 49.75f;
+    System.out.println("Your current Load Balance is: ₱" + loadBalance);
+
+    int roundedBalance = (int) loadBalance;
+
+    System.out.println("Rounded Load Balance(for promo checking): ₱" + roundedBalance);
+  }
+}
