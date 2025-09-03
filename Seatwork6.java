eb0aeb30a5014f9cbb95dd801bb816d98f63e17f
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-03 22:59:57 +1000

    Create Seatwork6.java

---
+++ Seatwork6.java
@@ -0,0 +1,7 @@
+public class Seatwork6 {
+  public static void main(String[] args) {
+    System.out.println("Seatwork No. 6");
+    String[] cars = {"Volvo","BMW","Ford","Mazda","Rusi","Honda","KiA"};
+    System.out.println(cars.length);
+  }
+}
