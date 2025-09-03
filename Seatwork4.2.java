efa094fbb031263e8c564ef361a2aae068c074e3
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-03 22:57:54 +1000

    Create Seatwork4.2.java

---
+++ Seatwork4.2.java
@@ -0,0 +1,12 @@
+public class Seatwork4_2 {
+  public static void main(String[] args) {
+    String introduction = "I am James Edward Ferrer, Student of TECHPRO A2. I have 370 in my Wallet.";
+
+    int i = 1;
+
+    do {
+      System.out.println(introduction);
+      i++
+    } while (i == 1);
+  }
+}
