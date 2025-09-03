a6290614b1b92414821876acea7417b89ce789b0
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-03 18:42:41 +1000

    Create Seatwork1Part2.java
    Seatwork 1 Part 2

---
+++ Seatwork1Part2.java
@@ -0,0 +1,10 @@
+public class Seatwork1Part2 {
+  public static void main(String[] args) {
+    double temperatureCelsius = 36.6;
+
+    int displayTemperature = (int) temperatureCelcius;
+
+    System.out.println("Original Temperature (Celcius): " + temperatureCelcius);
+    System.out.println("Displayed Temperature (Rounded): " + displayTemperature);
+  }
+}
