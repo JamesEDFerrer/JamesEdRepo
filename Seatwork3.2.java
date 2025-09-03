4ff474aa0aa852621c4f3e94ae1cad170fa4e67d
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-03 22:54:37 +1000

    Create Seatwork3.2.java

---
+++ Seatwork3.2.java
@@ -0,0 +1,11 @@
+public class Seatwork3_2 {
+  public static void main(String[] args) {
+    int doorCode = 1337;
+
+    if (doorcode == 1337) {
+      System.out.println("Correct code. The door is now open.");
+    } else {
+      System.out.println("Incorrect code The door remains closed.");
+    }
+  }
+}
