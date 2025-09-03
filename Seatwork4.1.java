ee1c814d5822e3dd53b5cc0b1a771235c40bb260
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-03 22:56:18 +1000

    Create Seatwork4.1.java

---
+++ Seatwork4.1.java
@@ -0,0 +1,12 @@
+public class Seatwork4_1 {
+  public static void main(String[] args) {
+    System.out.println("My name is James Edward Ferrer");
+
+    int i = 5;
+
+    while (i <= 15) {
+      System.out.println(i);
+      i++
+    }
+  }
+}
