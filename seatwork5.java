6507b2b6436d97183287b04dba9059c93caf55c2
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-08 14:04:18 +0800

    Create seatwork 5

---
+++ seatwork5.java
@@ -0,0 +1,13 @@
+public class seatWork5 {
+	public static void main(String[] args) {
+		System.out.println("Seatwork No. 5");
+		
+		int countdown = 3;
+		
+		while (countdown > 0) {
+			System.out.println(countdown);
+			countdown--;
+		}
+		System.out.println("Happy Birthday!");
+	}
+}
