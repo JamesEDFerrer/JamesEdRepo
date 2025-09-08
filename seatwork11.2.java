062511f3d0e998f4b3ff36ccd336b308f37baa8b
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-08 13:26:27 +0800

    Create seatwork11.2.java

---
+++ seatwork11.2.java
@@ -0,0 +1,9 @@
+public class seatWork11_2 {
+	public static void main(String[] args) {
+		System.out.println("Seatwork 11 Part 2");
+		
+		int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+		
+		System.out.println(myNumbers[1][2]);
+	}
+}
