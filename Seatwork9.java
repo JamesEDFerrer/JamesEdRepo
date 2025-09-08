ec4fd55a05e11c04f5792873e5e2f1cb8fb03cc9
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2025-09-08 13:52:20 +0800

    Create Seatwork 9

---
+++ Seatwork9.java
@@ -0,0 +1,12 @@
+public class seatWork9 {
+	public static void main(String[] args) {
+		int[] numbers = {10, 20, 30, 40};
+		int index = 0;
+		
+		while (index < 5) {
+			System.out.println("Value of item: " + numbers[index]);
+			index++;
+			System.out.println("\n");
+			}
+	}
+}
