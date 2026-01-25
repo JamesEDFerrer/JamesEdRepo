f5d5bbf0269c4e71583639937debb7bedfaa01a8
JamesEDFerrer <jamesedwardferrer.scc@gmail.com>
2026-01-25 13:56:43 +0000

    Uploaded Seatwork 7 A/B and 8

---
+++ ExceptionPartA.java
@@ -0,0 +1,17 @@
+// Sem 3 Seatwork 7 Part A
+
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 15;
+        int number2 = 0;
+
+        try {
+            int result = number 1 / number 2; // Error
+            System.out.println("Result:" + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception: Cannot divide by zero.");
+        }
+
+        System.out.println("Program continues to run...");
+    }
+}
---
+++ ExceptionPartB.java
@@ -0,0 +1,25 @@
+// Sem 3 Seatwork 7 Part B
+
+class PasswordException extends Exception {
+    public PasswordException(String message) {
+        super(message);
+    }
+}
+
+public class Main {
+    static void checkPassword(String password) throws PasswordException {
+        if (password.length() < 12) {
+            throw new PasswordException("Passkey must be 12 characters long.")
+        } else {
+            System.out.println("Passkey allowed.")
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            checkPassword("Earth");
+        } catch (PasswordException e) {
+            System.out.println("PasswordException: " + e.getMessage());
+        }
+    }
+}
